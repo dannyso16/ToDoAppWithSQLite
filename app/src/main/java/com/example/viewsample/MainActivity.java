67763485f4c7f022c505ddf161d3cd7365b19b07
dannyso16 <osatoshi.11@gmail.com>
2019-10-03 23:37:48 +0900

    adapterを自作のものに書き直した
    ポップアップも変えたよ
    DBの形式も見やすくなったよ
    別々にcommitするべき内容をまとめてるバカだよ

--- app/src/main/java/com/example/viewsample/MainActivity.java
+++ app/src/main/java/com/example/viewsample/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.viewsample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -22,12 +24,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etCompany;
-    private EditText etPrice;
+    private EditText etName;
+    private EditText etDetail;
     private DatabaseHelper2 helper;
-    private SimpleCursorAdapter adapter;
+    private ToDoAdapter adapter;
     private ArrayList<ToDoItem> toDoList;
-    // TODO: 2019/10/03 adapterをカスタムのものに置き換える 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // view の取得
-        etCompany = findViewById(R.id.et_company);
-        etPrice = findViewById(R.id.et_price);
+        etName = findViewById(R.id.et_name);
+        etDetail = findViewById(R.id.et_detail);
         ListView lvShow = findViewById(R.id.lv_show);
 
         // set button listener
@@ -56,113 +57,151 @@ public class MainActivity extends AppCompatActivity {
 
                 SQLiteDatabase db = helper.getWritableDatabase();
 
-                String key = etCompany.getText().toString();
-                String value = etPrice.getText().toString();
+                String name = etName.getText().toString();
+                String detail = etDetail.getText().toString();
 
-                if (key.isEmpty() | value.isEmpty()) {
+                // 空欄がある場合，再入力を促す
+                if (name.isEmpty()) {
                     Toast.makeText(getApplicationContext(),
                             "EditText is EMPTY !!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                insertData(db, key, value);
-
                 // 更新
-                Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
-                adapter.changeCursor(cursor);
-                adapter.notifyDataSetChanged();
+                ToDoItem item = new ToDoItem();
+                item.setName(name);
+                item.setDetail(detail);
+                insertData(item);
 
                 // 空欄に戻す
-                etCompany.setText("");
-                etPrice.setText("");
-                Log.d("debug", "INSERT btn is onCllck()");
+                etName.setText("");
+                etDetail.setText("");
+                Log.d("listener", "INSERT btn is onCllck()");
             }
         });
 
         // load db
         helper = new DatabaseHelper2(MainActivity.this);
         SQLiteDatabase db = helper.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
+        try {
+            Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
 
-        Log.d("debug", "db init # of columns " + cursor.getColumnCount());
-        Log.d("debug", "db init # of rows " + cursor.getCount());
+            toDoList = new ArrayList<>();
+            cursor.moveToFirst();
+            while (cursor.moveToNext()) {
+                ToDoItem item = new ToDoItem();
+                int idxName = cursor.getColumnIndex("name");
+                item.setName(cursor.getString(idxName));
+                int idxDetail = cursor.getColumnIndex("detail");
+                item.setDetail(cursor.getString(idxDetail));
+                toDoList.add(item);
+            }
+        } finally {
+            db.close();
+        }
+
 
 
         // adapter作成してListviewに適用
-        String[] from = {"company", "stockprice"};
-        int[] to = {android.R.id.text1, android.R.id.text2};
-
-        adapter = new SimpleCursorAdapter(MainActivity.this,
-                android.R.layout.simple_list_item_2,
-                cursor, from, to, 0);
-
+        adapter = new ToDoAdapter(MainActivity.this);
+        adapter.setToDoList(toDoList);
         lvShow.setAdapter(adapter);
 
-
         /*
-        ListView のリスナ
-            * short tap でインフォ
-            * long tap で削除
+         * ListView のリスナ
+            * 左の□：押すとそのtodoを削除
+            * 右の□：押すと「重要」になる
+            * 文字　：押すとポップアップで内容がでる
          */
         lvShow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("debug", "short clicked : id = " + i);
+            public void onItemClick(AdapterView<?> parent, View view, final int i, long l) {
+                // Log.d("listener", "view.getId(): " + view.getId());
+                ListView listView = (ListView)parent;
+                final ToDoItem item = (ToDoItem) listView.getItemAtPosition(i);
 
-                TextView tv1 = view.findViewById(android.R.id.text1);
-                String s1 = tv1.getText().toString();
-                TextView tv2 = view.findViewById(android.R.id.text2);
-                String s2 = tv2.getText().toString();
-                Toast.makeText(MainActivity.this,
-                        "country : " + s1 + "\nstockprice : " + s2,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+                switch (view.getId()) {
+                    case R.id.cb_done_in_lv:
+                        deleteData(item, i);
+                        break;
+                    case R.id.cb_star_in_lv:
+                        // TODO: 2019/10/03 starを付けた時の処理をかく
+                        break;
 
-        lvShow.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("debug", "Long clicked : _id = " + i);
+                    default:
+                        // ボタン以外の領域をタップしたとき(viewはなく-1が入るはず)
+                        // タップしたアイテムの取得
+                        Log.d("debug", "Tap: " + i);
 
-                TextView tv1 = view.findViewById(android.R.id.text1);
-                String s1 = tv1.getText().toString();
-                TextView tv2 = view.findViewById(android.R.id.text2);
-                String s2 = tv2.getText().toString();
-                deleteData(s1, s2);
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                        builder.setTitle("Tap No. " + i);
+//                        builder.setMessage(item.getName());
+//                        builder.show();
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle(item.getName())
+                                .setMessage(item.getDetail())
+                                .setPositiveButton("できたー", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Log.d("debug", "完了 is clicked");
+                                        deleteData(item, i);
 
-                SQLiteDatabase db = helper.getWritableDatabase();
-                Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
-                adapter.changeCursor(cursor);
-                adapter.notifyDataSetChanged();
+                                    }
+                                })
+                                .setNegativeButton("まだー", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Log.d("debug", "まだー is clicked");
+                                    }
+                                })
+                                .show();
+                        break;
 
-                Toast.makeText(MainActivity.this,
-                        "Selected Todo is deleted.",
-                        Toast.LENGTH_SHORT).show();
+                }
 
-                return true;
             }
         });
     }
     
 
-    private void insertData(SQLiteDatabase db, String company, String price) {
-        db.execSQL("INSERT INTO testdb (company, stockprice) VALUES (?, ?);",
-                new String[]{company, price});
-    }
 
-    private void deleteData(String company, String stockprice) {
+    private void insertData(ToDoItem item) {
+        String name = item.getName();
+        String detail = item.getDetail();
+        toDoList.add(item);
+
         SQLiteDatabase db = helper.getWritableDatabase();
-
         try {
-            db.execSQL("DELETE FROM testdb WHERE company=? AND stockprice=?;",
-                    new String[]{company, stockprice});
-            Log.d("debug", "deleteData() is called");
-            Log.d("debug", "name=" + company);
+            db.execSQL("INSERT INTO testdb (name, detail) VALUES (?, ?);",
+                new String[]{name, detail});
+            adapter.notifyDataSetChanged();
+            Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
+            Log.d("debug", "insertData() is called");
+            Log.d("debug", "rows : " + cursor.getCount());
         }
         finally {
             db.close();
         }
-
     }
 
+    private void deleteData(ToDoItem item, int index) {
+        String name = item.getName();
+        String detail = item.getDetail();
+        toDoList.remove(index);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        try {
+            db.execSQL("DELETE FROM testdb WHERE name=? AND detail=?;",
+                    new String[]{name, detail});
+            adapter.notifyDataSetChanged();
+
+            Cursor cursor = db.rawQuery("SELECT * FROM testdb", null);
+            Log.d("debug", "delteeData() is called");
+            Log.d("debug", "rows : " + cursor.getCount());
+
+        }
+        finally {
+            db.close();
+        }
+    }
 }
